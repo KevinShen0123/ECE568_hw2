c84523133a4980c1cd5a3dddcb53045fd50f97ce
KevinShen0123 <121901791+KevinShen0123@users.noreply.github.com>
2023-02-22 23:00:03 -0500

    Update Cache.hpp

--- Cache.hpp
+++ Cache.hpp
@@ -12,6 +12,6 @@ class Cache{
 	bool can_cache(std::string response);
 	bool check_expire(int id,std::string info,ServerResponse* response);
 	std::vector<std::string> get_cache_control(ServerResponse* response);
-	void   check_request_save(int server_fd,ClientRequest* request);
+	ServerResponse* check_request_save(int server_fd,ClientRequest* request);
 	void check_response_save(int server_fd,ClientRequest* request, ServerResponse* response);
 }; 
